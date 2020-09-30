540a6d068240489d16bc528f7378216df4bfae06
Joseph Ruiz <ruizj3@uw.edu>
2020-09-30 21:05:37 +0000

    Automatically backed up by Learn

--- top_movies_2010s/lib/top_movies_2010s.rb
+++
@@ -1,7 +0,0 @@
-require "nokogiri"
-require "pry"
-require "open-uri"
-
-require_relative "top_movies_2010s/version"
-require_relative './top_movies_2010s/cli'
-require_relative './top_movies_2010s/movies'
